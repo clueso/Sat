7f972d2790757a4aad61b1d6273d199ffb223e89
Ashwin <ashfixate@yahoo.co.uk>
2014-07-07 23:13:50 -0700

    First Commit: forms base for app.

---
+++ Sat/Sat.Windows/GenericFunctions.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Windows.Web.Http;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+static class GenericCodeClass
+{
+    public static void GetListOfURLs(List<string> FileNames, int NoOfFiles)
+    {
+            DateTime CurrDateTime = DateTime.Now.ToUniversalTime();
+            DateTime StartOfYearDate = new DateTime(CurrDateTime.Year-1, 12, 31);
+            TimeSpan NoOfDays = CurrDateTime.Subtract(StartOfYearDate);
+            int i;
+         
+
+            FileNames.Clear();
+
+            if (CurrDateTime.Minute < 30)
+                CurrDateTime = CurrDateTime.AddMinutes(0.0 - CurrDateTime.Minute);
+            else
+                CurrDateTime = CurrDateTime.AddMinutes(30.0 - CurrDateTime.Minute);
+
+            for (i = 0; i < NoOfFiles; i++)
+            {
+                FileNames.Add(CurrDateTime.Year.ToString() + NoOfDays.Days.ToString() + "_" + CurrDateTime.Hour.ToString("D2") + CurrDateTime.Minute.ToString("D2") + "vis.jpg");
+                CurrDateTime = CurrDateTime.AddMinutes(-30.0);
+                NoOfDays = CurrDateTime.Subtract(StartOfYearDate);
+            }
+
+            FileNames.Reverse();
+    }
+
+    public static async void DownloadFiles(List<string> Filenames, int NoOfFiles)
+    {
+        string URLPath = "http://www.ssd.noaa.gov/goes/west/wfo/sew/img/";
+        string FilePath;
+        //var URI = new Uri("http://www.ssd.noaa.gov/goes/west/wfo/sew/img/2014186_2030vis.jpg");
+        int i;
+        StorageFolder ImageFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("Images", CreationCollisionOption.OpenIfExists);
+
+
+        for (i = 0; i < NoOfFiles; i++)
+        {
+            FilePath = ImageFolder.Path + Filenames[i];
+
+            //if (!File.Exists(FilePath))
+            //{
+                try
+                {
+                    GetFileUsingHttp(URLPath + Filenames[i], ImageFolder, Filenames[i]);
+                }
+                catch
+                {
+                    Filenames[i] = "Error";
+                }
+            //}
+        }
+
+        //Filenames.RemoveAll(IsError);
+    }
+
+    public static async void GetFileUsingHttp(string URL, StorageFolder Folder, string FileName)
+    {
+        HttpClient Client = new HttpClient();
+        var URI = new Uri(URL);
+        HttpResponseMessage message = await Client.GetAsync(URI);
+
+        StorageFile sampleFile = await Folder.CreateFileAsync(FileName, CreationCollisionOption.ReplaceExisting);// this line throws an exception
+        var FileBuffer = await message.Content.ReadAsBufferAsync();
+
+        await FileIO.WriteBufferAsync(sampleFile, FileBuffer);
+    }
+
+    public static async Task<BitmapImage> GetBitmapImage(string FileName)
+    {
+        StorageFolder ImageFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("Images", CreationCollisionOption.OpenIfExists);
+        StorageFile ImageFile = await ImageFolder.CreateFileAsync(FileName, CreationCollisionOption.OpenIfExists);
+
+        BitmapImage Image = new BitmapImage();
+
+        Image = await LoadImage(ImageFile);
+
+        return Image;
+    }
+
+    private static async Task<BitmapImage> LoadImage(StorageFile file)
+    {
+        BitmapImage bitmapImage = new BitmapImage();
+        FileRandomAccessStream stream = (FileRandomAccessStream)await file.OpenAsync(FileAccessMode.Read);
+
+        bitmapImage.SetSource(stream);
+
+        return bitmapImage;
+
+    }
+
+    //public static void GetFileUsingHttp(string URL, string StorePath)
+    //{
+    //    HttpWebRequest Client = (HttpWebRequest) WebRequest.CreateHttp(URL);
+    //    HttpWebResponse ClientResp = (HttpWebResponse) Client.GetResponse();
+    //    BinaryReader ClientRespStream = new BinaryReader(ClientResp.GetResponseStream());
+    //    MemoryStream Image = new MemoryStream();
+    //    byte[] buffer;
+    //    FileStream ImgFile = new FileStream(StorePath, FileMode.Create);
+
+    //    buffer = ClientRespStream.ReadBytes(1024);
+
+    //    while (buffer.Length > 0)
+    //    {
+    //        Image.Write(buffer, 0, buffer.Length);
+    //        buffer = ClientRespStream.ReadBytes(1024);
+    //    }
+
+    //    byte[] lnFile = new byte[(int)Image.Length];
+    //    Image.Position = 0;
+    //    Image.Read(lnFile, 0, lnFile.Length);
+    //    Image.Close();
+    //    Image.Close();
+
+    //    ImgFile.Write(lnFile, 0, lnFile.Length);
+    //    ImgFile.Close();
+    //}
+
+    //public static void Cleanup()
+    //{
+    //    DirectoryInfo di = new DirectoryInfo("../Images");
+        
+    //    foreach (FileInfo file in di.GetFiles())
+    //    {
+    //        file.Delete();
+    //    }
+    //}
+
+    public static async void OverlayFiles(string Basefile, string OverlayFile)
+    {
+        StorageFolder ImageFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("Images", CreationCollisionOption.OpenIfExists);
+        StorageFile Base = await ImageFolder.CreateFileAsync(Basefile, CreationCollisionOption.OpenIfExists);
+        StorageFile Overlay = await ImageFolder.CreateFileAsync(OverlayFile, CreationCollisionOption.OpenIfExists);
+        var BaseImageStream = await Base.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+        var OverlayImageStream = await Overlay.OpenAsync(Windows.Storage.FileAccessMode.Read);
+        var BaseImageDecoder = await BitmapDecoder.CreateAsync(BaseImageStream);
+        var OverlayImageDecoder = await BitmapDecoder.CreateAsync(OverlayImageStream);
+        var BasePixelData = await BaseImageDecoder.GetPixelDataAsync();
+        var OverlayPixelData = await OverlayImageDecoder.GetPixelDataAsync();
+        var BasePixel = BasePixelData.DetachPixelData();
+        var OverlayPixel = OverlayPixelData.DetachPixelData();
+        int i, j;
+        uint height = BaseImageDecoder.PixelHeight, width = BaseImageDecoder.PixelWidth;
+
+        for (i = 0; i < height; i++)
+        {
+            for (j = 0; j < width; j++)
+            {
+                if (OverlayPixel[(i * width + j) * 4 + 0] > 0)
+                {
+                    BasePixel[(i * width + j) * 4 + 0] = OverlayPixel[(i * width + j) * 4 + 0];
+                    BasePixel[(i * width + j) * 4 + 1] = OverlayPixel[(i * width + j) * 4 + 1];
+                    BasePixel[(i * width + j) * 4 + 2] = OverlayPixel[(i * width + j) * 4 + 2];
+                    BasePixel[(i * width + j) * 4 + 3] = OverlayPixel[(i * width + j) * 4 + 3];
+                }
+            }
+        }
+
+        //InMemoryRandomAccessStream mem = new InMemoryRandomAccessStream();
+        var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, BaseImageStream);
+        encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Straight, width, height, BaseImageDecoder.DpiX, BaseImageDecoder.DpiY, BasePixel);
+        await encoder.FlushAsync();
+    }
+
+    public static bool IsError(string s)
+    {
+        return s.Equals("Error");
+    }
+}
---
+++ Sat/Sat.Windows/MainPage.xaml.cs
@@ -0,0 +1,174 @@
+﻿using Sat.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace Sat
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        List<string> Files = new List<string>(6);
+        private static int CurrImgIndex = -1;
+        private static int EndIndex = 0;
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation. Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session. The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void QuitButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            App.Current.Exit();
+        }
+
+        private async void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrImgIndex != -1 && Files.Count != 0)
+            {
+                CurrImgIndex = ++CurrImgIndex % Files.Count;
+                ImgBox.Source = await GenericCodeClass.GetBitmapImage(Files[CurrImgIndex]);
+                //ImgBox.Source = await GenericCodeClass.GetBitmapImage("2014186_1730vis.jpg");
+                //MapBox.ImageLocation = URLPath + Files[CurrImgIndex];
+                StatusBox.Text = "CurrImgIndex = " + CurrImgIndex.ToString() + "::" + Files[CurrImgIndex].ToString();
+            }
+            StatusBox.Text = "You clicked Next Button";
+
+            GenericCodeClass.OverlayFiles("test.jpg", "overlay.jpg");
+        }
+
+        private async void PrevButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrImgIndex != -1 && Files.Count != 0)
+            {
+                CurrImgIndex = (CurrImgIndex + Files.Count - 1) % Files.Count;
+                ImgBox.Source = await GenericCodeClass.GetBitmapImage(Files[CurrImgIndex]);
+                //ImgBox.Source = await GenericCodeClass.GetBitmapImage("2014186_1700vis.jpg");
+                //MapBox.ImageLocation = URLPath + Files[CurrImgIndex];
+            }
+
+            StatusBox.Text = "CurrImgIndex = " + CurrImgIndex.ToString() + "::" + Files[CurrImgIndex].ToString();
+        }
+
+        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            //StorageFolder installedLocation = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            int i;
+
+            GenericCodeClass.GetListOfURLs(Files, 6);
+
+            for (i = 0; i < Files.Count; i++)
+            {
+                StatusBox.Text = string.Concat(StatusBox.Text, Files[i]);
+                StatusBox.Text = string.Concat(StatusBox.Text, Environment.NewLine);
+            }
+
+            GenericCodeClass.DownloadFiles(Files, 6);
+
+            if (Files.Count > 1)
+            {
+                CurrImgIndex = 0;
+                //ImgBox.Source = GenericCodeClass.GetBitmapImage(Files[CurrImgIndex]);
+                //MapBox.ImageLocation = URLPath + Files[CurrImgIndex];
+            }
+            else
+                CurrImgIndex = -1;
+            //StatusBox.Text = "You clicked Download Button";
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Exit();
+        }
+    }
+}
